8f3a9d856023fab96e0cf2b898edc57f82516de1
O-belus <devanshnair0112358@gmail.com>
2025-01-04 00:01:36 +0530

    Deploy Next.js site to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  trailingSlash: true, // Ensures proper static file paths
+  assetPrefix: '/<repository-name>/',
 };
 
 export default nextConfig;
